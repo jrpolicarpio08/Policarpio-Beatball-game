ccbc70fc26fe52570fa53cb2b1fd162acf69a7fa
jrpolicarpio08 <56585695+jrpolicarpio08@users.noreply.github.com>
2022-01-04 00:36:15 +0800

    beatball progress 1

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    public int hits = 1;
+    public int points = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        hits--;
+        if (hits <=0) {
+            Destroy(gameObject);
+        }
+    }
+}
